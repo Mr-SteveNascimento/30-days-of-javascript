7454e16685e7d78ee6ba5f9516203b8dddd57444
SteveNascimento <stevecraken@gmail.com>
2021-08-26 09:20:29 -0300

    Day-10

--- day-10/exercise-lvl-1.js
+++ day-10/exercise-lvl-1.js
@@ -2,9 +2,57 @@
 // Exercícios do dia 10 do curso 30 dias de javascript
 console.log('Exercícios nível 1\n\n');
 
+const a = [4, 5, 8, 9]
+const b = [3, 4, 5, 7]
+const countries = ['Finland', 'Sweden', 'Norway']
+
 // =========> 1 <=========
 console.log('\n\n Questão 1 \n\n ');
 
+const set1 = new Set();
+console.log(set1);
 
 // =========> 2 <=========
 console.log('\n\nQuestão 2 \n\n ');
+
+let set2 = new Set();
+for (let i = 0; i <= 10; i++) {
+    set2.add(i);
+}
+console.log(set2);
+
+
+// =========> 3 <=========
+console.log('\n\nQuestão 3 \n\n ');
+
+let set3 = new Set([...a]);
+console.log(set3);
+set3.delete(8)
+console.log(set3);
+
+// =========> 4 <=========
+console.log('\n\nQuestão 4 \n\n ');
+
+let set4 = new Set([...a]);
+console.log(set4);
+set4.clear()
+console.log(set4);
+
+// =========> 5 <=========
+console.log('\n\nQuestão 5 \n\n ');
+
+let set5 = new Set(countries);
+console.log(set5);
+set5.add('Brasil');
+set5.add('Canada');
+console.log(set5);
+
+
+// =========> 6 <=========
+console.log('\n\nQuestão 6 \n\n ');
+
+let map1 = new Map();
+countries.forEach(element => {
+    map1.set(element, element.length)
+})
+console.log(map1);
--- day-10/exercise-lvl-2.js
+++ day-10/exercise-lvl-2.js
@@ -5,6 +5,27 @@ console.log('Exercícios nível 2\n\n');
 // =========> 1 <=========
 console.log('\n\n Questão 1 \n\n ');
 
+u = [...a, ...b];
+
+console.log(a);
+console.log(b);
+console.log(u);
 
 // =========> 2 <=========
 console.log('\n\nQuestão 2 \n\n ');
+
+let i = a.filter(element  => b.includes(element));
+console.log(i);
+
+// =========> 3 <=========
+console.log('\n\nQuestão 3 \n\n ');
+
+let w = new Map();
+if (a.length === b.length) {
+    for (let i = 0; i < a.length; i++) {
+        w.set(a[i], b[i]);       
+    }
+    console.log(w);
+} else {
+    alert('Questão 3 Nível 2\nOs Arrays não possuem o mesmo tamanho!')
+}
